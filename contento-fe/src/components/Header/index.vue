00c2e7259d83ed5f10fc38f764763d5d013af375
Nguyen Xuan Vinh <Vinhnxse62725@gmail.com>
2019-09-28 16:11:33 +0700

    Add Lib

--- contento-fe/src/components/Header/index.vue
+++ contento-fe/src/components/Header/index.vue
@@ -5,14 +5,27 @@
         class="headline text-uppercase"
         style="display: flex;
     justify-content: center;
-    align-items: center;">
-      <span>Contento</span>
+    align-items: center;"
+      >
+        <img src="@/assets/Logo.png" style="height: 90%;" />
+        <span>Contento</span>
       </v-toolbar-title>
-      <div style="min-width:30px; width:100px;"/>
+      <div style="min-width:30px; width:100px;" />
       <v-btn text to="/">Trending</v-btn>
       <v-btn text to="/profile">Video</v-btn>
-      <v-btn text>Category</v-btn>
-
+      <!-- <v-btn text>Category</v-btn> -->
+      <v-menu bottom max-height="200" min-width="400"  max-width="400" offset-y="true" origin="center center" transition="scale-transition">
+        <template v-slot:activator="{ on }">
+          <v-btn text v-on="on">Category</v-btn>
+        </template>
+        <div v-bar>
+        <v-list>
+          <v-list-item style="float: left;  width:50%; background: white;" v-for="(item, i) in items" :key="i" @click>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+        </div>
+      </v-menu>
     </v-toolbar-items>
     <v-text-field hide-details prepend-icon="mdi-magnify" style="min-width:100px;"></v-text-field>
     <v-spacer></v-spacer>
@@ -27,7 +40,25 @@
   </v-app-bar>
 </template>
 <script>
-export default {};
+export default {
+  data: () => ({
+    items: [
+      { title: "Topic 1" },
+      { title: "Topic 2" },
+      { title: "Topic 3" },
+      { title: "Topic 4" },
+      { title: "Topic 5" },
+      { title: "Topic 6" },
+      { title: "Topic 7" },
+      { title: "Topic 8" },
+      { title: "Topic 9" },
+      { title: "Topic 10" },
+      { title: "Topic 11" },
+      { title: "Topic 12" }
+    ]
+  })
+};
 </script>
 <style scoped>
+
 </style>
--- contento-fe/src/main.js
+++ contento-fe/src/main.js
@@ -2,7 +2,8 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router/router";
 import store from "./store/store";
-import vuetify from './plugins/vuetify';
+import vuetify from './plugins/vuetify/index';
+import vuebar from "./plugins/vuebar/index";
 
 Vue.config.productionTip = false;
 
@@ -10,5 +11,6 @@ new Vue({
   router,
   store,
   vuetify,
+  vuebar,
   render: h => h(App)
 }).$mount("#app");
